5fdf1e0d0f5cfa15c5e12f98254fde4e75dfdb62
brunograna <brunogarciarana@gmail.com>
2024-03-10 12:28:14 -0300

    feat: add admin endpoint to list users

--- src/main/java/tech/buildrun/security/controller/UserController.java
+++ src/main/java/tech/buildrun/security/controller/UserController.java
@@ -1,6 +1,7 @@
 package tech.buildrun.security.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import tech.buildrun.security.controller.dto.CreateUserDto;
@@ -9,6 +10,7 @@ import tech.buildrun.security.domain.User;
 import tech.buildrun.security.repository.RoleRepository;
 import tech.buildrun.security.repository.UserRepository;
 
+import java.util.List;
 import java.util.Set;
 
 @RestController
@@ -36,6 +38,15 @@ public class UserController {
                 User.fromCreateUserDto(createUserDto, Set.of(defaultRole), bCryptPasswordEncoder));
 
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
+    public ResponseEntity<List<User>> listUsers() {
+
+        var users = userRepository.findAll();
+
+        return ResponseEntity.ok(users);
     }
 
 
